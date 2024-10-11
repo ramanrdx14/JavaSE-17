913fe2822875f0d2e7013cdc3d0ef5dd0113ea59
ramanrdx14 <ramanrs1410@gmail.com0>
2024-10-11 12:15:48 -0700

    Data Type

---
+++ 2.DataType/Main.java
@@ -0,0 +1,16 @@
+class Main{
+	public static void main(String[] args) {
+		byte a = 100;   	 // -127 to +127
+		short s = 100; 		//  -32768 to +32767
+		int i  = 100; 	   //   -2147483648 to 2147483647
+		long l = 100L;    //    -10^18      to 10^18
+		float f = 100.121f;
+		double d = 109232.23;
+		System.out.println(a);
+
+		char male = 'M';
+		boolean isMale = true;
+		boolean isFemale= false;
+		printbreak;
+	}
+}
